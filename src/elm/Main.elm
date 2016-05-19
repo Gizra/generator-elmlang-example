0b71a09a0b5607ee691879dcd4788d06ef1f4f74
Amitai Burstein <amitai@gizra.com>
2016-05-19 20:08:42 +0300

    better styling

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -7,4 +7,8 @@ import Html.App as Html
 main : Program Never
 main =
   Html.program
-    { init = init, update = update, view = view, subscriptions = \_ -> Sub.none }
+    { init = init
+    , update = update
+    , view = view
+    , subscriptions = \_ -> Sub.none
+    }
